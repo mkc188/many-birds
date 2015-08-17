c2c43248c42f9c18c444882b90f3bab50d6d54fe
Peter Lam <peter@retepmal.com>
2015-08-17 16:01:43 +0800

    Database schema changed due to FB SDK upgrade

--- database/manybirds.sql
+++ database/manybirds.sql
@@ -1,17 +1,10 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `reteazbp_manybirds`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `achievement`
@@ -305,7 +298,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
   `updated` char(25) NOT NULL,
-  `access_token` varchar(255) NOT NULL,
   `friend_list_time` int(11) NOT NULL,
   `friend_list_etag` varchar(255) NOT NULL,
   PRIMARY KEY (`uid`)
